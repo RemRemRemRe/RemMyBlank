b5f307ed8925552c18bee12b3d3fb7bf5817b627
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-09-12 01:50:43 +0800

    Changed: adopt REM_API style

--- Source/RemMyBlank/Public/RemMyBlankLog.h
+++ Source/RemMyBlank/Public/RemMyBlankLog.h
@@ -3,9 +3,10 @@
 #pragma once
 
 #include "Logging/LogMacros.h"
+#include "Macro/RemMacroUtilities.h"
 
 #define REM_API REMMYBLANK_API
 
-REM_API DECLARE_LOG_CATEGORY_EXTERN(LogRemMyBlank, Log, All);
+REM_API DECLARE_LOG_CATEGORY_EXTERN(LogRemMyBlank, REM_DEFAULT_LOG_VERBOSITY, REM_MAX_LOG_VERBOSITY);
 
 #undef REM_API
